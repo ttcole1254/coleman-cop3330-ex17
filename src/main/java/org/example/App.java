6e0289ad7ce71a78dda36eda758e2fe53893b074
Test <Test@test.com>
2021-09-13 22:38:16 -0400

    Added assignment code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,41 @@
 package org.example;
 
-/**
- * Hello world!
- *
+import java.util.Objects;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 16 Solution
+ *  Copyright 2021 Tyler Coleman
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a 1 if you are male or a 2 if you are female: ");
+        int gender = in.nextInt();
+        System.out.println("How many ounces of alcohol did you have? ");
+        double ounces = in.nextDouble();
+        System.out.println("What is your weight, in pounds? ");
+        double weight = in.nextDouble();
+        System.out.println("How many hours has it been since your last drink? ");
+        double hours = in.nextDouble();
+        double r = 0;
+        if (gender == 1) {
+            r = 0.73;
+        }
+        else if (gender == 2) {
+            r = 0.66;
+        }
+        double bac = (ounces * 5.14 / weight * r) - 0.015 * hours;
+        System.out.printf("Your BAC is %f\n", bac);
+        if (bac <= 0.08) {
+            System.out.println("Is it legal for you to drive,");
+        }
+        else {
+            System.out.println("It is not legal for you to drive.");
+        }
     }
 }
